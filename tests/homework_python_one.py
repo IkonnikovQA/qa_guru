36e7cdad09338d2435d818ef1666b5baacfbc475
Ikonnikov <Ikonnikov.Oleg47@gmail.com>
2023-06-14 21:27:30 +0300

    add test_circle

--- tests/homework_python_one.py
+++ tests/homework_python_one.py
@@ -1,3 +1,4 @@
+import math
 
 
 def test_greeting():
@@ -36,11 +37,11 @@ def test_circle():
     """
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = math.pi * r ** 2
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2 * math.pi * r
     assert length == 144.51326206513048
 
 
